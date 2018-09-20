5a39b6cf2349bb8d06e18802c5eeae17d11c2ad7
Jonathan Valldejuli <jvalldejuli7@gmail.com>
2018-09-20 19:51:53 -0400

    Set the LoginActivity instance as the listener of the button. Prints the entered email and password when LoginButton is tapped.

--- app/src/main/java/com/fiveguys/cs2340/drackr/LoginActivity.java
+++ app/src/main/java/com/fiveguys/cs2340/drackr/LoginActivity.java
@@ -2,12 +2,39 @@ package com.fiveguys.cs2340.drackr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText emailField;
+    private EditText passwordField;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailField = (EditText) findViewById(R.id.emailField);
+        passwordField = (EditText) findViewById(R.id.passwordField);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this);
+
     }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginButton:
+                String loginMessage = String.format("Username: %s Password: %s", emailField.getText().toString(), passwordField.getText().toString());
+                Log.i("User login", loginMessage);
+                break;
+            default:
+                Log.d("Unknown view clicked.", "Unknown view clicked.");
+        }
+    }
+
 }
