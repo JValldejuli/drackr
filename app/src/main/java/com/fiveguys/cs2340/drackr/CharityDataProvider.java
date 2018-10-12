4a460d52eb1f761f6bbdb2c1f64af25c9ceec387
Jonathan Valldejuli <jvalldejuli7@gmail.com>
2018-10-12 19:24:14 -0400

    Wrote CharityDataProvider.

---
+++ app/src/main/java/com/fiveguys/cs2340/drackr/CharityDataProvider.java
@@ -0,0 +1,58 @@
+package com.fiveguys.cs2340.drackr;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.siegmar.fastcsv.reader.CsvReader;
+import de.siegmar.fastcsv.reader.CsvRow;
+import de.siegmar.fastcsv.reader.CsvParser;
+
+public class CharityDataProvider {
+
+    private ArrayList<Charity> charities = new ArrayList<Charity>();
+
+    public CharityDataProvider(InputStream inputStream) {
+
+        CSVFile csvFile = new CSVFile(inputStream);
+        List charityDataObjects = csvFile.read();
+        charityDataObjects.remove(0);
+
+        for (Object charityDataObject : charityDataObjects) {
+            String[] charityData = (String[]) charityDataObject;
+
+            CharityType type = CharityType.charityType(charityData[8]);
+
+            URL url;
+            try {
+                url = new URL(charityData[10]);
+            } catch(Exception e) {
+                url = null;
+            }
+
+            Charity charity = new Charity(Integer.parseInt(charityData[0]),
+                    charityData[1],
+                    Double.parseDouble(charityData[2]),
+                    Double.parseDouble(charityData[3]),
+                    charityData[4],
+                    charityData[5],
+                    charityData[6],
+                    Integer.parseInt(charityData[7]),
+                    type,
+                    charityData[9],
+                    url);
+            charities.add(charity);
+        }
+
+    }
+
+    public ArrayList<Charity> getCharities() {
+        return charities;
+    }
+
+}
