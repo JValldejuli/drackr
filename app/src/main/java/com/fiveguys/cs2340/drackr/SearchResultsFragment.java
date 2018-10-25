aae71f69e9bc12c75586547fb46608d222b7a8dd
Jonathan Valldejuli <jvalldejuli7@gmail.com>
2018-10-25 18:03:09 -0400

    Wrote SearchResultsFragment.

---
+++ app/src/main/java/com/fiveguys/cs2340/drackr/SearchResultsFragment.java
@@ -0,0 +1,57 @@
+package com.fiveguys.cs2340.drackr;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+public class SearchResultsFragment extends Fragment {
+
+    private ListSelectionDelegate mListener;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public SearchResultsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_donations, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            recyclerView.setAdapter(new BasicRecyclerViewAdapter(DonationSearchCoordinator.results, mListener));
+        }
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof ListSelectionDelegate) {
+            mListener = (ListSelectionDelegate) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnListFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+}
