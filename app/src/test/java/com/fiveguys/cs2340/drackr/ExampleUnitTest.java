4b04829686988c7dede6a4bac870dfbbe54d1449
Jonathan Valldejuli <jvalldejuli7@gmail.com>
2018-11-07 15:24:04 -0500

    Wrote tests for SearchCoordinator.

--- app/src/test/java/com/fiveguys/cs2340/drackr/ExampleUnitTest.java
+++ app/src/test/java/com/fiveguys/cs2340/drackr/ExampleUnitTest.java
@@ -2,7 +2,11 @@ package com.fiveguys.cs2340.drackr;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
+import java.util.Date;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -33,12 +37,44 @@ public class ExampleUnitTest {
 
     @Test
     public void testRegisterValidArgs() {
-        UserAccount a = new UserAccount("s", "d", "f");r
+        UserAccount a = new UserAccount("s", "d", "f");
         UserAuthenticator.registerUserAccount("s", "d", "f");
         UserAccount signedInUserAfter = UserAuthenticator.getSignedInUserAccount();
         assertEquals(signedInUserAfter.getName(), a.getName());
         assertEquals(signedInUserAfter.getEmail(), a.getEmail());
         assertEquals(signedInUserAfter.getPassword(), a.getPassword());
+    }
+
+    private Charity makeCharity() {
+        List<Donation> a = new ArrayList<Donation>();
+        a.add(new Donation(new Date(), "2", "shirt", 0, DonationType.CLOTHING));
+        a.add(new Donation(new Date(), "2", "computer", 0, DonationType.ELECTRONICS));
+        return new Charity("", "", 1, 1, "", "", "", 0, CharityType.DROP_OFF, "", "", a);
+    }
+
+    @Test
+    public void testSetSpecificCharity() {
+        Charity c = makeCharity();
+        DonationSearchCoordinator.setSpecificCharity(c);
+        assertEquals(c, DonationSearchCoordinator.getSpecificCharity());
+    }
+
+    @Test
+    public void testSpecificCharityDonationTypeSearch() {
+        Charity c = makeCharity();
+        DonationSearchCoordinator.setSpecificCharity(c);
+        DonationSearchCoordinator.searchDonationsByType(DonationType.CLOTHING);
+        Donation result = DonationSearchCoordinator.getResults().get(0);
+        assertEquals(result, c.getDonations().get(0));
+    }
+
+    @Test
+    public void testSpecificCharityDescriptionSearch() {
+        Charity c = makeCharity();
+        DonationSearchCoordinator.setSpecificCharity(c);
+        DonationSearchCoordinator.searchDonationsByDescription("computer");
+        Donation result = DonationSearchCoordinator.getResults().get(0);
+        assertEquals(result, c.getDonations().get(1));
     }
 
 }
